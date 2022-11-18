0804f9b91c3e4dccd95047b31c8a3ccb49df7da0
usman <shabbir.usman3314@gmail.com>
2022-11-18 20:37:10 +0500

    object

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-//variable
-let name="usman"; //string
-let age=22; //number
-let numer=14.6; //number
-let a=true; //boolean
-let lastname=null; //clear storage null
-console.log(name ,age ,numer,a);
-//constant 
-const amount=12;
-console.log(amount);
+// // variable
+// let name="usman"; //string
+// let age=22; //number
+// let numer=14.6; //number
+// let a=true; //boolean
+// let lastname=null; //clear storage null
+// console.log(name ,age ,numer,a);
+// //constant 
+// const amount=12;
+// console.log(amount);
+
+//object
+let car={
+    color: 'black',
+    model: 2022
+};
+// dot notation
+car.color='red';
+// bracket notion
+let select='model';
+car[select]=2020;
+console.log(car);
+
