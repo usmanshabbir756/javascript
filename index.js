d53835218a9ba85144061ebf88b5909c2b7ef5da
usman <shabbir.usman3314@gmail.com>
2022-11-18 20:20:13 +0500

    const and premitive type

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //variable
-let name="usman";
-let age=22;
-let numer=14.6;
-let a=true;
+let name="usman"; //string
+let age=22; //number
+let numer=14.6; //number
+let a=true; //boolean
+let lastname=null; //clear storage null
 console.log(name ,age ,numer,a);
+//constant 
+const amount=12;
+console.log(amount);
